isaacs/node-v0.x-archive

Add history-search-backward and history-search-forward functionality.
Make up and down scroll through history entries prefixed with entered text.
Ex: history contains (ls,pwd,echo "foo",ps aux)
type: p(UP) --> scrolls through "ps aux" and "pwd", skips echo and ls.

Like putting this in an .inputrc file:
"\e[A": history-search-backward
"\e[B": history-search-forward
"\e[C": forward-char
"\e[D": backward-char

lib/readline.js
@@ -236,7 +236,44 @@ function commonPrefix(strings) {
   return min;
 }
 
+Interface.prototype._searchNext = function () {
+  this._buildSearch();
+  if (this.search.index > 0) {
+    this.search.index --;
+    this.line = this.search[this.search.index];
+    this._refreshLine();
+  }
+}
+Interface.prototype._searchPrev = function () {
+  this._buildSearch();
+  if (this.search.index + 1 < this.search.length) {
+    this.search.index++;
+    this.line = this.search[this.search.index];
+    this._refreshLine();
+  }
+}
+Interface.prototype._buildSearch = function () {
+  // if the search hasn't changed, then don't touch it.
+  if (this.search && this.search.length &&
+      this.search.cursor === this.cursor &&
+      this.search.query === this.line.substr(0, this.cursor)) return;
+  var s = this.line.substr(0, this.cursor);
+  var uniq = [];
+  this.search = this.history.filter(function (h) {
+    var ok = h.indexOf(s) === 0 && uniq.indexOf(h) === -1;
+    uniq.push(h);
+    return ok;
+  });
+  this.search.query = s;
+  this.search.cursor = this.cursor;
+  this.search.index = -1;
+}
 Interface.prototype._historyNext = function () {
+  if (this.cursor !== 0
+      && !(this.cursor === this.line.length && this.historyIndex > -1)
+      && process.env.NODE_READLINE_SEARCH) {
+    return this._searchNext();
+  }
   if (this.historyIndex > 0) {
     this.historyIndex--;
     this.line = this.history[this.historyIndex];
@@ -250,8 +287,12 @@ Interface.prototype._historyNext = function () {
     this._refreshLine();
   }
 };
-
 Interface.prototype._historyPrev = function () {
+  if (this.cursor !== 0
+      && !(this.cursor === this.line.length && this.historyIndex >= 0)
+      && process.env.NODE_READLINE_SEARCH) {
+    return this._searchPrev();
+  }
   if (this.historyIndex + 1 < this.history.length) {
     this.historyIndex++;
     this.line = this.history[this.historyIndex];
@@ -291,6 +332,7 @@ Interface.prototype._ttyWrite = function (b) {
       this.output.write('\n\x1b[0G');
       stdio.setRawMode(false);
       this.emit('line', line);
+      this.search = null;
       break;
 
     case 127:   /* backspace */

dazagrohovaz/node

lib/readline.js
@@ -408,8 +408,47 @@ Interface.prototype._line = function() {
   this._onLine(line);
 };
 
+Interface.prototype._searchNext = function() {
+  this._buildSearch();
+  if (this.search.index > 0) {
+    this.search.index --;
+    this.line = this.search[this.search.index];
+    this._refreshLine();
+  }
+}
+
+Interface.prototype._searchPrev = function() {
+  this._buildSearch();
+  if (this.search.index + 1 < this.search.length) {
+    this.search.index++;
+    this.line = this.search[this.search.index];
+    this._refreshLine();
+  }
+}
+
+Interface.prototype._buildSearch = function() {
+  // if the search hasn't changed, then don't touch it.
+  if (this.search && this.search.length &&
+      this.search.cursor === this.cursor &&
+      this.search.query === this.line.substr(0, this.cursor)) return;
+  var s = this.line.substr(0, this.cursor);
+  var uniq = [];
+  this.search = this.history.filter(function(h) {
+    var ok = h.indexOf(s) === 0 && uniq.indexOf(h) === -1;
+    uniq.push(h);
+    return ok;
+  });
+  this.search.query = s;
+  this.search.cursor = this.cursor;
+  this.search.index = -1;
+}
 
 Interface.prototype._historyNext = function() {
+  if (this.cursor !== 0
+      && !(this.cursor === this.line.length && this.historyIndex > -1)
+      && process.env.NODE_READLINE_SEARCH) {
+    return this._searchNext();
+  }
   if (this.historyIndex > 0) {
     this.historyIndex--;
     this.line = this.history[this.historyIndex];
@@ -426,6 +465,11 @@ Interface.prototype._historyNext = function() {
 
 
 Interface.prototype._historyPrev = function() {
+  if (this.cursor !== 0
+      && !(this.cursor === this.line.length && this.historyIndex >= 0)
+      && process.env.NODE_READLINE_SEARCH) {
+    return this._searchPrev();
+  }
   if (this.historyIndex + 1 < this.history.length) {
     this.historyIndex++;
     this.line = this.history[this.historyIndex];